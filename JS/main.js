1a4dc3f44edb19fe3774fe8963bf6fb47ca2ea05
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-13 16:34:32 +0200

    get hours, seconds, ...

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,19 @@
 const MAIN_TITLE = document.getElementById('main-title');
 
 const NOW = new Date();
-console.log(NOW);
+
+//probabilmente da mettere dentro una funzione
+const HOURS = NOW.getHours();
+const MINUTES = NOW.getMinutes();
+const SECONDS = NOW.getSeconds();
+const MILLISECONDS = NOW.getMilliseconds();
+
+if (SECONDS < 10 && MILLISECONDS > 10 && MILLISECONDS < 100) {
+  MAIN_TITLE.innerHTML = (`${HOURS}:${MINUTES}:0${SECONDS}:0${MILLISECONDS}`);
+} else if (SECONDS < 10 && MILLISECONDS < 10) {
+  MAIN_TITLE.innerHTML = (`${HOURS}:${MINUTES}:0${SECONDS}:00${MILLISECONDS}`);
+} else if (SECONDS < 10) {
+  MAIN_TITLE.innerHTML = (`${HOURS}:${MINUTES}:0${SECONDS}:${MILLISECONDS}`);
+} else {
+  MAIN_TITLE.innerHTML = (`${HOURS}:${MINUTES}:${SECONDS}:${MILLISECONDS}`);
+}
